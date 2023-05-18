f728bdfae29ab7285f683c6ba9cc4c499283e88f
Devmuliya16 <muliyadev16@gmail.com>
2023-05-18 13:57:15 +0530

    add authentication with firebase created login signup page and config folder for firebase craditionals

---
+++ components/auth/signup.js
@@ -0,0 +1,53 @@
+import { useState } from "react"
+import {auth} from "../../config/firebase";
+import { createUserWithEmailAndPassword,signInWithPopup,GoogleAuthProvider } from "firebase/auth";
+
+function page() {
+    //form handelers
+    const [data,_data] = useState({name:"",email:"",password:""})
+    const change =(e)=>{
+        _data({...data,[e.target.name]:e.target.value});
+    }
+
+
+    //sign in
+    const signIn = async ()=>{
+        var message;
+        const response = await createUserWithEmailAndPassword(auth,data.email,data.password)
+        .catch((e)=>{message=e.message});
+        message = response ?? message;
+        window.alert(message)
+    }
+
+    //google signup
+    const googleSignup = async ()=>{
+      const provider = new GoogleAuthProvider();
+      signInWithPopup(auth,provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+        console.log(user,token);
+      }).catch((e) => {
+        window.alert(e.message)
+      });
+    
+    }
+  
+  return (
+    // just for testing ui 
+    <form action=""  onSubmit={(e)=>e.preventDefault()}>
+        <span>Signup page</span><br/>
+        email:
+        <input type="email" onChange={change} name="email" value={data.email} required/>
+        password:
+        <input type="text" onChange={change} name="password" value={data.password} required/>
+        <button type="submit" onClick={signIn}>signup</button>
+        <button onClick={googleSignup}>Signup with google</button>
+    </form>
+  )
+}
+
+export default page
+
+//write code to print hello world
